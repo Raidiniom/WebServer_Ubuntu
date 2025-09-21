a7cfb602d751e2b6e59306fe53156ada3d5cc69f
raidiniom <raidiniom22ph@gmail.com>
2025-09-22 01:40:57 +0800

    hehe

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 const { register, login } = require("../controllers/authController");
 
+// Serve registration page
+router.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/register.html"));
+});
+
+// Handle registration form submission
 router.post("/register", register);
+
+// Serve login page
+router.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+});
+
+// Handle login form submission
 router.post("/login", login);
 
 module.exports = router;
