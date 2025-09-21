2b7826cce0c9fa58c87ae4e87678382c2ba50ca2
raidiniom <raidiniom22ph@gmail.com>
2025-09-22 01:28:28 +0800

    hehe

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,16 +1,22 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 
 function isAuthenticated(req, res, next) {
     if (req.session.user) {
         return next();
     } else {
-        res.redirect("/login");
+        res.redirect("/auth/login");
     }
 }
 
 router.get("/", isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, "../public/dashboard.html"));
 });
+
+router.get("/user", isAuthenticated, (req, res) => {
+    res.json({ username: req.session.user.username });
+});
+
 
 module.exports = router;
