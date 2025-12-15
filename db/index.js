8d99d3e3d7fcc54e82cda581668b441b76f1dfb3
raidiniom <raidiniom22ph@gmail.com>
2025-12-15 13:03:00 +0800

    Final

--- db/index.js
+++ db/index.js
@@ -1,10 +1,6 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DB_NAME:", process.env.DB_NAME);
-
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
